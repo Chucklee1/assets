2fe6d921c43616f11d8e35fe58b74fadc2e9579a
Chucklee1 <91444983+Chucklee1@users.noreply.github.com>
2024-12-12 01:21:00 +0000

    typo, part not label for root

--- flake.nix
+++ flake.nix
@@ -7,7 +7,13 @@
     pkgs = import nixpkgs { system = system; };
     scriptName = "lazyInstaller";
     buildInputs = with pkgs; [ parted nixos-install nixos-enter ];
-    scriptDef = pkgs.writeShellScriptBin scriptName ./test.sh;
+    scriptDef = writeShellApplication scriptName {
+      name = "show-nixos-org";
+      runtimeInputs = [ parted ];
+      text = ''
+        curl -s 'https://nixos.org' | w3m -dump -T text/html
+      '';
+    };
 
   in {
     defaultPackage.${system} = self.packages.${system}.${script-name};
--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ BOOT_SIZE="500MB"
 parted /dev/${DISK} -- mklabel gpt
 parted /dev/${DISK} -- mkpart ESP 1MB ${BOOT_SIZE}
 parted /dev/${DISK} -- set 1 esp on
-parted /dev/${DISK} -- mklabel root ${BOOT_SIZE} 100%
+parted /dev/${DISK} -- mkpart root ${BOOT_SIZE} 100%
